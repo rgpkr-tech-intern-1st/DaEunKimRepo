2e81419b79eb90e8061db349c12906ba49643803
rgpkorea <rgplocaladm@MacBook-Pro-2.local>
2018-09-10 01:14:16 +0900

    숨겨진 수 찾는 방법2 코드 개선

--- 01.guess/find_number.py
+++ 01.guess/find_number.py
@@ -1,27 +1,34 @@
-import random
 import subprocess
 import sys
 import time
 
 
-message = ['The value is greater than your guess.\n', 'The value is less than your guess.\n']
-result = message[0]
-
-min = sys.maxint * (-1) -1
-max = sys.maxint
-
-start_time = time.time()
-while result in message:
-    num = random.randrange(min, max+1)
-    print num
-    result = subprocess.check_output(['python', '__main__.pyc', str(num)])
-    if result==message[0]:
-        min = num + 1
-    else:
-        max = num
-end_time = time.time()
+def find_by_binary_search(**args):
+    min_ = args['min_']
+    max_ = args['max_']
+    while True:
+        num = (min_ + max_) / 2
+        print num
+        result = subprocess.check_output(['python', 'guess.zip', str(num)])
+        if 'greater' in result:
+            min_ = num + 1
+        elif 'less' in result:
+            max_ = num - 1
+        else:
+            break
+    return num
 
 
+def count_seconds(function_, **args):
+    start_time = time.time()
+    yield function_(**args)
+    end_time = time.time()
+    yield end_time - start_time
 
-print "The hidden number is {}!".format(num)
-print "Time to find the number takes {} seconds.".format(end_time - start_time)
+
+if __name__ == '__main__':
+    min_ = -sys.maxint - 1
+    max_ = sys.maxint
+    num, time_ = count_seconds(find_by_binary_search, min_=min_, max_=max_)
+    print 'The hidden number is {0}!'.format(num)
+    print 'It takes {0} seconds.'.format(time_)
