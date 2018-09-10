5a62115a25ed101331e37a26af166d76461873d2
rgpkorea <rgplocaladm@MacBook-Pro-2.local>
2018-09-10 12:33:54 +0900

    함수명 짧게 변경

--- 01.guess/find_number.py
+++ 01.guess/find_number.py
@@ -3,19 +3,19 @@ import sys
 import time
 
 
-def find_by_binary_search(min_, max_):
+def find(min_, max_):
     num = (min_ + max_) / 2
     print num
     result = subprocess.check_output(['python', 'guess.zip', str(num)])
     if 'greater' in result:
-        return find_by_binary_search(num+1, max_)
+        return find(num+1, max_)
     elif 'less' in result:
-        return find_by_binary_search(min_, num-1)
+        return find(min_, num-1)
     else:
         return num
 
 
-def count_seconds(func, min_, max_):
+def count(func, min_, max_):
     start_time = time.time()
     num = func(min_, max_)
     end_time = time.time()
@@ -25,6 +25,6 @@ def count_seconds(func, min_, max_):
 if __name__ == '__main__':
     min_ = -sys.maxint - 1
     max_ = sys.maxint
-    num, time_ = count_seconds(find_by_binary_search, min_, max_)
+    num, time_ = count(find, min_, max_)
     print 'The hidden number is {}!'.format(num)
     print 'It takes {} seconds.'.format(time_)
