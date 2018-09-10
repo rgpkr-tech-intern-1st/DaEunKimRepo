a17ef94fb028cc47d63857aedbf99546568e522b
rgpkorea <rgplocaladm@MacBook-Pro-2.local>
2018-09-10 11:34:55 +0900

    탐색방법 재귀로 변경, yield 제거

--- 01.guess/find_number.py
+++ 01.guess/find_number.py
@@ -3,32 +3,28 @@ import sys
 import time
 
 
-def find_by_binary_search(**args):
-    min_ = args['min_']
-    max_ = args['max_']
-    while True:
-        num = (min_ + max_) / 2
-        print num
-        result = subprocess.check_output(['python', 'guess.zip', str(num)])
-        if 'greater' in result:
-            min_ = num + 1
-        elif 'less' in result:
-            max_ = num - 1
-        else:
-            break
-    return num
+def find_by_binary_search(min_, max_):
+    num = (min_ + max_) / 2
+    print num
+    result = subprocess.check_output(['python', 'guess.zip', str(num)])
+    if 'greater' in result:
+        return find_by_binary_search(num+1, max_)
+    elif 'less' in result:
+        return find_by_binary_search(min_, num-1)
+    else:
+        return num
 
 
-def count_seconds(function_, **args):
+def count_seconds(func, min_, max_):
     start_time = time.time()
-    yield function_(**args)
+    num = func(min_, max_)
     end_time = time.time()
-    yield end_time - start_time
+    return num, end_time - start_time
 
 
 if __name__ == '__main__':
     min_ = -sys.maxint - 1
     max_ = sys.maxint
-    num, time_ = count_seconds(find_by_binary_search, min_=min_, max_=max_)
-    print 'The hidden number is {0}!'.format(num)
-    print 'It takes {0} seconds.'.format(time_)
+    num, time_ = count_seconds(find_by_binary_search, min_, max_)
+    print 'The hidden number is {}!'.format(num)
+    print 'It takes {} seconds.'.format(time_)
