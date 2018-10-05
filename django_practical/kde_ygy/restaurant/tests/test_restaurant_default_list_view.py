d233fd66ff5ea7ed27c3fdd55647f90c4da96ec9
DaEun <kde6260@gmail.com>
2018-10-05 17:00:25 +0900

    restaurant 테스트코드 뼈대 생성

---
+++ django_practical/kde_ygy/restaurant/tests/test_restaurant_default_list_view.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+
+class RestaurantDefaultListViewTests(TestCase):
+    def test_default_list_view_should_return_restaurants_with_same_region_when_request_with_region(self):
+        pass
+
+    def test_default_list_view_should_return_restaurants_with_same_postal_code_when_request_with_postal_code(self):
+        pass
+
+    def test_default_list_view_should_return_restaurants_opened_when_compared_to_current_time(self):
+        pass
+
+    def test_default_list_view_should_return_message_when_no_restaurants_exists(self):
+        pass
