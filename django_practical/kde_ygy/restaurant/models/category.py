6c4d25903ff55b9116e12ee9e17272493bc241b1
DaEun <kde6260@gmail.com>
2018-10-07 00:46:16 +0900

    음식점 종류 모델에 메타데이터 추가

--- django_practical/kde_ygy/restaurant/models/category.py
+++ django_practical/kde_ygy/restaurant/models/category.py
@@ -38,5 +38,9 @@ class Category(models.Model):
         ],
     )
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return f'{self.name}-{self.id}'
