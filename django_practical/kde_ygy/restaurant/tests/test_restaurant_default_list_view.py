8ea97b4c6370746ef25f3e7f87b99d78e7d22189
DaEun <kde6260@gmail.com>
2018-10-07 00:45:55 +0900

    restaurant 디폴트 리스트 우편번호, 영업시간 테스팅하는 코드 추가

--- django_practical/kde_ygy/restaurant/tests/test_restaurant_default_list_view.py
+++ django_practical/kde_ygy/restaurant/tests/test_restaurant_default_list_view.py
@@ -1,15 +1,142 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.utils import timezone
+
+from ..models import Restaurant
 
 
 class RestaurantDefaultListViewTests(TestCase):
-    def test_default_list_view_should_return_restaurants_with_same_region_when_request_with_region(self):
+    @classmethod
+    def create_later_time(cls, now):
+        return (now + timezone.timedelta(minutes=1)).strftime('%H:%M:%S')
+
+    @classmethod
+    def create_earlier_time(cls, now):
+        return (now - timezone.timedelta(minutes=1)).strftime('%H:%M:%S')
+
+    def test_default_list_view_should_return_restaurants_with_local_postal_code_when_request_without_postal_code(self):
         pass
 
-    def test_default_list_view_should_return_restaurants_with_same_postal_code_when_request_with_postal_code(self):
-        pass
+    def test_default_list_view_should_only_return_restaurants_with_same_postal_code_when_request_with_postal_code(self):
+        # Given
+        postal_code = '01234'
+        same_postal_code_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기치킨-강남점',
+            address='서울시 요기구 요기동',
+            postal_code='01234',
+            minimum_order_price=14000,
+            open_time='00:00:00',
+            close_time='23:59:59',
+        )
+        different_postal_code_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기짜장-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01233',
+            minimum_order_price=14000,
+            open_time='00:00:00',
+            close_time='23:59:59',
+        )
 
-    def test_default_list_view_should_return_restaurants_opened_when_compared_to_current_time(self):
-        pass
+        # When
+        response = self.client.get(reverse('restaurant:default_list', kwargs={
+            'postal_code': postal_code,
+        }))
 
-    def test_default_list_view_should_return_message_when_no_restaurants_exists(self):
-        pass
+        # Then
+        self.assertQuerysetEqual(
+            response.context['restaurants'], [
+                f'<Restaurant: {same_postal_code_restaurant}>',
+            ],
+        )
+
+    def test_default_list_view_should_only_return_opened_restaurants_when_compared_to_current_time(self):
+        # Given
+        postal_code = '01234'
+        present_time = timezone.localtime()
+        not_yet_opened_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기피자-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01234',
+            minimum_order_price=14000,
+            open_time=self.create_later_time(present_time),
+            close_time=timezone.localtime().max.strftime('%H:%M:%S'),
+        )
+        already_closed_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기카페-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01234',
+            minimum_order_price=14000,
+            open_time=timezone.localtime().min.strftime('%H:%M:%S'),
+            close_time=self.create_earlier_time(present_time),
+        )
+        opened_restaurant = Restaurant.objects.create(
+            name='요기버거-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01234',
+            minimum_order_price=14000,
+            open_time=self.create_earlier_time(present_time),
+            close_time=self.create_later_time(present_time),
+        )
+
+        # When
+        response = self.client.get(reverse('restaurant:default_list', kwargs={
+            'postal_code': postal_code,
+        }))
+
+        # Then
+        self.assertQuerysetEqual(
+            response.context['restaurants'], [
+                f'<Restaurant: {opened_restaurant}>',
+            ],
+        )
+
+    def test_default_list_view_should_return_empty_when_restaurants_satisfying_conditions_do_not_exists(self):
+        # Given
+        postal_code = '01234'
+        present_time = timezone.localtime()
+        different_postal_code_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기짜장-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01233',
+            minimum_order_price=14000,
+            open_time='00:00:00',
+            close_time='23:59:59',
+        )
+        not_yet_opened_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기피자-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01234',
+            minimum_order_price=14000,
+            open_time=self.create_later_time(present_time),
+            close_time=timezone.localtime().max.strftime('%H:%M:%S'),
+        )
+        already_closed_restaurant = Restaurant.objects.create(
+            is_franchise=True,
+            name='요기카페-서초점',
+            address='서울시 요기구 기요동',
+            postal_code='01234',
+            minimum_order_price=14000,
+            open_time=timezone.localtime().min.strftime('%H:%M:%S'),
+            close_time=self.create_earlier_time(present_time),
+        )
+
+        # When
+        response = self.client.get(reverse('restaurant:default_list', kwargs={
+            'postal_code': postal_code,
+        }))
+
+        # Then
+        self.assertQuerysetEqual(
+            response.context['restaurants'], [
+            ],
+        )
+        self.assertContains(
+            response,
+            '주문가능한 음식점이 제공되지 않습니다.',
+        )
